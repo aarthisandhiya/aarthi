48020d15d2f48b655abca155def879674527196a
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-01-08 15:27:27 +0530

    Update great.py

--- great.py
+++ great.py
@@ -1,6 +1,10 @@
-s=int(input())
-y=int(input())
-c=int(input())
+my1=raw_input()
+my = my1.split(" ")
+
+s = int(my[0])
+y = int(my[1])
+c = int(my[2])
+
 if s>y and s>c:
 	print(s)
 elif y>s and y>c:
