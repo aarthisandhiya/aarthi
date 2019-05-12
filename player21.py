67c858488e37e282b7b332ada1dc34377c858003
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-12 07:12:37 +0530

    Update player21.py

--- player21.py
+++ player21.py
@@ -2,13 +2,18 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 c=list(map(int,input().split()))
 count=0
+d=0
+j=1 
 for i in range (0,2):
     if a[i]==b[i]==c[i]:
         count=count+1 
-        break
+for i in range (0,2):
+    if a[i]==a[j] and b[i]==b[j] and c[i]==c[j]:
+        d=d+1
+            
         
         
-if count>0:
+if count>0 or d>0:
     print("yes")
 else:
     print("no")
