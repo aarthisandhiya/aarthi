ee42c071ebd0f623489f822a11901c4d1dfd55fb
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-19 13:17:05 +0530

    Update hunter3.py

--- hunter3.py
+++ hunter3.py
@@ -3,11 +3,11 @@ c=[]
 count=0
 b=[int(a) for a in input().split()]
 for i in range(0,len(b)):
-    if b[i]==int(i):
+    if b[i]==i:
         c.append(i)
         count=count+1 
 if count>0:
     for i in c:
-        print(i,end="")
+        print(i,end=" ")
 else:
     print("-1")
