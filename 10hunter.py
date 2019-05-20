77011e7cae389c4a8f37411e4ae74f48e3b4f05c
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-20 09:53:40 +0530

    Create 10hunter.py

---
+++ 10hunter.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+c=0
+n=[int(a) for a in input().split()]
+m=[int(b) for b in input().split()]
+for i in range(0,len(m)):
+    for j in range(0,len(n)):
+        if m[i]==n[j]:
+            c=c+1 
+if c==len(m):
+    print("yes")
+else:
+    print("no")
