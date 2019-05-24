34563ba2a155f7a34b15c7e4395d0e6e827aa958
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-24 11:22:57 +0530

    Update 101hunter.py

--- 101hunter.py
+++ 101hunter.py
@@ -4,9 +4,10 @@ for i in range(2,a):
     for j in range(2,i):
         for k in range(2,a):
             for l in range(2,k):
-                if i%j!=0 and i%k!=0 and i+j+k==a:
+                if i%j!=0 and k%l!=0 and i+j+k==a:
                     s.append(j)
                     s.append(i)
                     s.append(k)
+                    break
 print(s[0],s[1],s[2])
             
