c13343781c273f19366862854b925e883304eb9f
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-25 06:08:22 +0530

    Update 72hunter.py

--- 72hunter.py
+++ 72hunter.py
@@ -1,6 +1,16 @@
 aa=input().split()
+s=''
 for i in range(0,len(aa)):
-    if i%2==0:
+    if i==int(len(aa)-1):
+        d=list(aa[i])
+        for j in d:
+            if j!='.':
+                s=s+str(j)+''
+        if i%2==0:
+            print(s[::-1],end=" ")
+        elif i%2==1:
+            print(s,end=" ")
+    elif i%2==0:
         print(aa[i][::-1],end=" ")
     elif  i%2==1:
         print(aa[i],end=" ")
