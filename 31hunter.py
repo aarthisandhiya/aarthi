c716710e8b219c9afa73a49ea10d749b568237ab
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-27 23:56:17 +0530

    Update 31hunter.py

--- 31hunter.py
+++ 31hunter.py
@@ -3,15 +3,15 @@ b=list(map(int,input().split()))
 k=[]
 for i in range(0,a-1):
   n,n1=b[:i+1],b[i+1:]
-  pr1,pr2=1,1
+  prod1,prod2=1,1
   #print(n,n1)
   for i in n:
-    pr1*=i
+    prod1*=i
   for i in n1:
-    pr2*=i
-  if pr1>pr2:
-    k.append(pr1)
+    prod2*=i
+  if prod1>prod2:
+    k.append(prod1)
   else:
-    k.append(pr2)
-print(l)
-print(max(l))
+    k.append(prod2)
+#print(l)
+print(max(k))
