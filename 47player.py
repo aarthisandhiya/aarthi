99c7d62841bcf50fddd73f091592201868f98cc4
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-29 14:38:28 +0530

    Update 47player.py

--- 47player.py
+++ 47player.py
@@ -1,14 +1,10 @@
 a,b,c=map(int,input().split())
-s=[]
-s.append(a)
-s.append(b)
-s.append(c)
-e=0
-for i in range(0,len(s)):
-    if s[i]!=0:
-        w=int(((s[i]-2)*180)/s[i])
-        e=e+w
-if e<=180:
-    print("yes")
+if a!=0 and b!=0 and c!=0:
+    if a+b+c==180:
+        print("yes")
+    else:
+        print("no")
 else:
     print("no")
+        
+    
