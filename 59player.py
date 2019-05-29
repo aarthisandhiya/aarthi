c68f3d19ee5611cf6142113b1b886cf3c3490f79
aarthisandhiya <46442329+aarthisandhiya@users.noreply.github.com>
2019-05-29 16:49:55 +0530

    Create 59player.py

---
+++ 59player.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=[int(a) for a in input()]
+#print(b)
+s=[]
+for i in range(0,len(b)):
+    if b[i]==1:
+        #print(b[i])
+        d=b[:i]
+       # print(d)
+        for k in d:
+            if k!=0:
+                kit=0
+                s.append(k)
+print(*s[0:6])
